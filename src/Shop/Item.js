ab12241f9dcd5fcfb7cf5fe8a22cce29c0868d4c
jjjjonathan <hornathanjon@gmail.com>
2021-03-24 14:30:11 -0500

    add quantity dropdown

--- src/Shop/Item.js
+++ src/Shop/Item.js
@@ -6,6 +6,16 @@ export default function Item(props) {
       <h4 className="item-title">{props.title}</h4>
       <p className="item-description">{props.description}</p>
       <p className="price">${props.price}</p>
+      <label htmlFor="quantity">Qty:</label>
+
+      <select name="quantity" id="quantity">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+      </select>
       <button
         data-index={props.index}
         type="button"
