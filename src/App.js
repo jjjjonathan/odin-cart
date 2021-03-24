2d44a6abe92b6f9c158b5b5e8a8e2e43a216b666
jjjjonathan <hornathanjon@gmail.com>
2021-03-23 19:21:17 -0500

    add Home and Shop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home/Home.js";
+import Shop from "./Shop/Shop.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/shop">Shop</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/shop" component={Shop} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
