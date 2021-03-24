65a992851738a68005d620c8c9c50f7c5f142a54
jjjjonathan <hornathanjon@gmail.com>
2021-03-23 22:01:53 -0500

    lift items state up to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import React, { useState } from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Home from "./Home/Home.js";
 import Shop from "./Shop/Shop.js";
 import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      title: "Odinfeathers",
+      description: "Faux feathers in pastel gradients",
+      price: 595,
+      quantityInCart: 0,
+    },
+    {
+      title: "The Nondescript Odinstone",
+      description: "You expected description? See above",
+      price: 9995,
+      quantityInCart: 0,
+    },
+    {
+      title: "Ode in Car",
+      description: "The hit single. Vinyl single only",
+      price: 39,
+      quantityInCart: 0,
+    },
+    {
+      title: "Odincart",
+      description:
+        "Virtual shopping cart. Contains no real products nor is it itself a real product",
+      price: 299,
+      quantityInCart: 0,
+    },
+  ]);
+
   return (
     <BrowserRouter>
       <div>
@@ -21,7 +50,9 @@ function App() {
         </header>
         <main>
           <Switch>
-            <Route path="/shop" component={Shop} />
+            <Route path="/shop">
+              <Shop items={items} />
+            </Route>
             <Route path="/" component={Home} />
           </Switch>
         </main>
--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -1,31 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import Item from "./Item";
 
 export default function Shop(props) {
-  const [items, setItems] = useState([
-    {
-      title: "Odinfeathers",
-      description: "Faux feathers in pastel gradients",
-      price: 595,
-    },
-    {
-      title: "The Nondescript Odinstone",
-      description: "You expected description? See above",
-      price: 9995,
-    },
-    {
-      title: "Ode in Car",
-      description: "The hit single. Vinyl single only",
-      price: 39,
-    },
-    {
-      title: "Odincart",
-      description:
-        "Virtual shopping cart. Contains no real products nor is it itself a real product",
-      price: 299,
-    },
-  ]);
-
   return (
     <div>
       <div>
@@ -33,7 +9,7 @@ export default function Shop(props) {
         <p className="lead">Remember, none of this is real.</p>
       </div>
       <div className="shop">
-        {items.map((item) => {
+        {props.items.map((item) => {
           return <Item title={item.title} description={item.description} />;
         })}
       </div>
