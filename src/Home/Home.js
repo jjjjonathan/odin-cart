063d88c89da25c11082f82f0921c0fccca1a838d
jjjjonathan <hornathanjon@gmail.com>
2021-03-23 20:20:43 -0500

    add Home.css

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,11 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home(props) {
-  return <h2>Home</h2>;
+  return (
+    <div>
+      <h1>Welcome to Odinstore.</h1>
+      <p className="lead">Where you can't buy anything, but you can pretend.</p>
+    </div>
+  );
 }
