18bb04fea423345c8d3df85414f5860fd6146a06
jjjjonathan <hornathanjon@gmail.com>
2021-03-25 12:20:55 -0500

    tweak spacing on items and cart

--- src/Shop/Item.js
+++ src/Shop/Item.js
@@ -13,22 +13,28 @@ export default function Item(props) {
   const CartStatus = (props) => {
     if (props.quantityInCart === 0) {
       return (
-        <form noValidate onSubmit={props.onAddToCart} data-index={props.index}>
-          <label htmlFor="quantity">Qty:</label>
-          <select name="quantity" id="quantity">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-          </select>
-          <button type="submit">Add to cart</button>
-        </form>
+        <div className="item-form">
+          <form
+            noValidate
+            onSubmit={props.onAddToCart}
+            data-index={props.index}
+          >
+            <label htmlFor="quantity">Qty:</label>
+            <select name="quantity" id="quantity">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+            </select>
+            <button type="submit">Add to cart</button>
+          </form>
+        </div>
       );
     } else {
       return (
-        <div>
+        <div className="item-form">
           <p className="success">Added {props.quantityInCart} to cart!</p>
           <button
             type="button"
