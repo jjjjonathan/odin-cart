08da6ab44caf7f14b6c6dbee58546bd76543e332
jjjjonathan <hornathanjon@gmail.com>
2021-03-24 23:17:08 -0500

    add checkout button and total

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -6,17 +6,33 @@ export default function Cart(props) {
         {props.cartItems.map((item) => {
           return (
             <div className="cart-item" key={item.index}>
+              <div className="cart-item-total-price">
+                ${item.quantityInCart * item.price}
+              </div>
               <div className="cart-item-title">{item.title}</div>
-              <div className="cart-item-each-price">${item.price}</div>
+              <div className="cart-item-each-price">${item.price} each</div>
               <div className="cart-item-quantity">
                 Qty: {item.quantityInCart}
-              </div>
-              <div className="cart-item-total-price">
-                Total: ${item.quantityInCart * item.price}
+                {/* <label htmlFor="quantity">Qty:</label>
+                <select name="quantity" id="quantity">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                </select> */}
               </div>
             </div>
           );
         })}
+        <div className="cart-total">
+          Total: $
+          {props.cartItems.reduce((accumulator, item) => {
+            return accumulator + item.quantityInCart * item.price;
+          }, 0)}
+          <button type="button">Checkout</button>
+        </div>
       </div>
     </div>
   );
