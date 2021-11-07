be859941e0ddd432a2ece81bb5cfaa81b4d1fc08
jjjjonathan <hornathanjon@gmail.com>
2021-11-07 01:20:13 -0700

    Refactor Home component for TypeScript

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react';
 
-export default function Home(props) {
+const Home = () => {
   return (
     <div>
       <h2>Welcome to Odincart.</h2>
       <p className="lead">Where you can't buy anything, but you can pretend.</p>
     </div>
   );
-}
+};
+
+export default Home;
